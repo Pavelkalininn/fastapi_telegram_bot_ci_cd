1c2a564d944c0cfb77908131a0385f5b0a49eb6f
Pavelel Kalinin <mono-gluk@ya.ru>
2022-08-05 18:15:58 +0300

    start workflow creation

--- telegram_bot_api/test_api.py
+++ telegram_bot_api/test_api.py
@@ -1,14 +1,25 @@
 from fastapi.testclient import TestClient
-from main import app
+from main import app, BREAD, IS_HAVE_EARS, HELP, NOT_UNDERSTAND
+
 
 client = TestClient(app)
+test_user = 33344455566
 
 
 def test_send_message():
-    response = client.get('/33444555/test_text/')
+    response = client.get(f'/{test_user}/start/')
     assert response.status_code == 200
-    assert response.json().get('telegram_id') == 33444555
-    assert response.json().get('text') == 'test_text'
+    assert response.json() == HELP
+    second_response = client.get(f'/{test_user}/дА/')
+    assert second_response.json() == IS_HAVE_EARS
+    third_response = client.get(f'/{test_user}/НеТ/')
+    assert third_response.json() == BREAD
+
+
+def test_send_wrong_text():
+    response = client.get(f'/{test_user}/test_text/')
+    assert response.status_code == 200
+    assert response.json() == NOT_UNDERSTAND
 
 
 def test_bad_telegram_id():
