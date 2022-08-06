4998e71a98de9132510e8576a0530316abf5a866
Pavelel Kalinin <mono-gluk@ya.ru>
2022-08-06 18:45:53 +0300

    Bot deploy

--- telegram_bot_api/database.py
+++ telegram_bot_api/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(os.environ.get(
     "DATABASE_URL",
-    default='sqlite:///db_file.db')
+    default='sqlite://db_file.db')
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Model = declarative_base()
